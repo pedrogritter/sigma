5117a67085b86e7cccb43874e01edafad3a26a15
Pedro Gritter <pedro12345g@gmail.com>
2019-03-07 17:07:15 +0000

    Bug: Import pages app in urls.py

--- sigma/urls.py
+++ sigma/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, pages
+from django.contrib import admin
 from django.urls import include, path
+ 
 
 urlpatterns = [
-    path('', include(pages.urls)),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 
 ]
