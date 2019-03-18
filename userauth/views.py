e4bb7be639d7a12bef7ba5355ce55ab58b3e13ac
Pedro Gritter <pedro12345g@gmail.com>
2019-03-18 00:38:56 +0000

    Minor fix to form. Include success messages.

--- userauth/views.py
+++ userauth/views.py
@@ -9,8 +9,15 @@ from django.views.generic import CreateView
 
 #Jsut for testing
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('landing')
     form = UserCreationForm()
     return render(request, 'userauth/signup.html', {'form': form})
 
