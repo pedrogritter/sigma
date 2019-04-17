fb64f7dc698ca45e5aa7f1bef0bc37a1f861b3e9
Pedro Gritter <pedro12345g@gmail.com>
2019-04-17 14:15:59 +0100

    debug

--- sigma/settings.py
+++ sigma/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_9k6j!c((d^fda8x6oy@os=42bzu1^zm$7c0=sq!*))y5-1+yp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
