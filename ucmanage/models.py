7c2c98544f3ed158b9cd34b036d2439549c9adc2
Gonçalo Sobral <g.sobral96@gmail.com>
2019-04-23 19:35:01 +0100

    Created ucmanage lib

---
+++ ucmanage/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+class Faculdade(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+
+class Departamento(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    fac = models.ForeignKey("Faculdade", on_delete=models.CASCADE, blank=True,null=True)
+
+class Curso(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    abv = models.CharField(max_length=10, blank=True, null=True)
+    dep = models.ForeignKey("Departamento", on_delete=models.CASCADE, blank=True,null=True)
+
+class UnidadeCurricular(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    abv = models.CharField(max_length=10, blank=True, null=True)
+    cursoID = models.ForeignKey("Curso", on_delete=models.CASCADE, blank=True,null=True)
+    #regente
+    year = models.IntegerField()
+
+class Turno(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    abv = models.CharField(max_length=10, blank=True, null=True)
+    ucID =  models.ForeignKey("UnidadeCurricular", on_delete=models.CASCADE, blank=True,null=True)
+
+class Aula(models.Model):
+    TYPE_AULA = (('Lab'),('Pratica'),('Teorica'))
+    DAY_CHOICES = (('Segunda'),('Terça'),('Quarta'),('Quinta'),('Sexta'),('Sabado'), ('Domingo'))
+    type = models.CharField(max_length=1, choices=TYPE_AULA)
+    turnoID = models.ForeignKey("Turno", on_delete=models.CASCADE, blank=True,null=True)
+    horaINI = models.TimeField()
+    horaFIM = models.TimeField()
+    diaSemana = models.CharField(max_length=1, choices=DAY_CHOICES)
+
+class Presença(models.Model):
+    aulaID = models.ForeignKey("Aula", on_delete=models.CASCADE, blank=True,null=True)
+    alunoID = models.ForeignKey(settings.Profile, on_delete=models.CASCADE, blank=True,null=True)
+    date = models.DateField()
