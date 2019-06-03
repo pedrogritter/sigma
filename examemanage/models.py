071fd4111ba1633ad3c1bb585ee595a724a7a94d
DiogoCatalao <writerd2013@hotmail.com>
2019-06-03 17:29:03 +0100

    exame

--- examemanage/models.py
+++ examemanage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from userprofiles.models import Profile
+from ucmanage.models import *
 
 # Create your models here.
+class Exame(models.Model):
+    ucID = models.ForeignKey('ucmanage.UnidadeCurricular', on_delete=models.CASCADE, blank=True,null=True)
+    horaINI = models.TimeField()
+    Duracao = models.TimeField()
+    date = models.DateField()
+
+
+
+
+class ExameAluno(models.Model):
+    exameID = models.ForeignKey("Exame", on_delete=models.CASCADE, blank=True,null=True)
+    alunoID = models.ForeignKey('userprofiles.Profile', on_delete=models.CASCADE, blank=True,null=True)
