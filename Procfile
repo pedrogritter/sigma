c3dee40158df624e099f936cf982e91623867598
Pedro Gritter <pedro12345g@gmail.com>
2019-06-24 14:12:08 +0100

    Gunicorn Workers config: - Concurency 3 workers

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
-web: gunicorn sigma.wsgi
+#web: gunicorn sigma.wsgi
 
 # Procfile with nginx, pgbouncer, gunicorn and django-q
-web: bin/start-nginx bin/start-pgbouncer-stunnel gunicorn -c gunicorn.conf sigma.wsgi:application
+web: bin/start-nginx bin/start-pgbouncer-stunnel gunicorn -c gunicorn.conf sigma.wsgi
 worker: bin/start-pgbouncer-stunnel python manage.py qcluster
--- sigma/settings.py
+++ sigma/settings.py
@@ -91,13 +91,6 @@ WSGI_APPLICATION = 'sigma.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
