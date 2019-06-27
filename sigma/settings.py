076b4ba7699b2727d54a06640358f71be02cccff
Pedro Gritter <pedro12345g@gmail.com>
2019-06-27 20:21:58 +0100

    comment static cdn

--- sigma/settings.py
+++ sigma/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 
 COMPRESS_PRECOMPILERS = (
