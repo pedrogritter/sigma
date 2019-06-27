bb93d726f53ca64e685be846d0d242e45a3d9d4a
Pedro Gritter <pedro12345g@gmail.com>
2019-06-27 20:13:48 +0100

    edit

--- sigma/settings.py
+++ sigma/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '_9k6j!c((d^fda8x6oy@os=42bzu1^zm$7c0=sq!*))y5-1+yp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sigma-alfa.herokuapp.com']
 
 
 # Application definition
