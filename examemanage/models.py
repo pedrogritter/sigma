3f2b1bcdc956d0e4120dd8e8795b0ecfbc54f9e9
Pedro Gritter <pedro12345g@gmail.com>
2019-06-28 04:04:46 +0100

    exam fix

--- examemanage/models.py
+++ examemanage/models.py
@@ -5,13 +5,13 @@ from ucmanage.models import UnidadeCurricular
 
 # Create your models here.
 class Exame(models.Model):
-    ucID = models.ForeignKey('ucmanage.UnidadeCurricular', blank=True,null=True)
+    ucID = models.ForeignKey('ucmanage.UnidadeCurricular', on_delete=models.CASCADE, blank=True,null=True)
     horaINI = models.TimeField()
     horaFim = models.TimeField()
     date = models.DateField()
 
 
 class ExameAluno(models.Model):
-    exameID = models.ForeignKey("examemanage.Exame", blank=True,null=True)
-    alunoID = models.ForeignKey('userprofiles.Profile', blank=True,null=True)
+    exameID = models.ForeignKey("examemanage.Exame", on_delete=models.CASCADE, blank=True,null=True)
+    alunoID = models.ForeignKey('userprofiles.Profile', on_delete=models.CASCADE, blank=True,null=True)
     inscrito = models.BooleanField(default=False)
